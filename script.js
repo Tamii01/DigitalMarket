bca79ab6dc2a88c86cd0f66d7f63dbb523e73cc5
Lautaro Colella <lautarocolella1@gmail.com>
2024-05-22 06:14:12 -0300

    animacion busqueda navbar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+	document.documentElement.style.opacity = '1';
 	handleBusqueda();
 	mostrarBotonUsuario();
 	iniciarCarouselOwl();
@@ -6,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleBusqueda() {
 	const busquedaMostrar = document.querySelector('#listBusquedaNav label');
+	const busquedaIcono = document.querySelector('#listBusquedaNav label i');
 	const busquedaInput = document.querySelector('#listBusquedaNav input');
 	const busquedaBoton = document.querySelector('#listBusquedaNav button');
 	const busquedaForm = document.querySelector('#listBusquedaNav form');
@@ -14,26 +16,50 @@ function handleBusqueda() {
 	const usuario = document.querySelector('nav div:last-child ul li:nth-child(3)');
 
 	busquedaMostrar.addEventListener('click', () => {
-		busquedaMostrar.style.display = 'none';
-		carrito.style.display = 'none';
-		if (usuario) usuario.style.display = 'none';
-		busquedaInput.style.display = 'block';
-		busquedaBoton.style.display = 'block';
+		busquedaIcono.style.transform = 'rotate(-360deg) scaleX(0.3)';
+		busquedaIcono.style.backgroundColor = "#fff"
+
+		setTimeout(() => {
+			busquedaIcono.style.transform = 'scaleX(0.3) translateX(500px)';
+			setTimeout(() => {
+				carrito.style.display = 'none';
+				if (usuario) usuario.style.display = 'none';
+					setTimeout(() => {
+						busquedaIcono.style.display = 'none';
+						busquedaInput.style.display = 'block';
+						busquedaBoton.style.display = 'block';
+						setTimeout(() => {
+							busquedaMostrar.style.display = 'none';
+							busquedaInput.style.opacity = '1';
+							busquedaBoton.style.opacity = '1';
+							busquedaInput.focus();
+
+						}, 300);
+				}, 300);
+			}, 120);
+		}, 500);
 	});
 
 	busquedaInput.addEventListener('blur', () => {
 		setTimeout(() => {
-			busquedaInput.style.display = 'none';
-			busquedaBoton.style.display = 'none';
-			busquedaMostrar.style.display = 'inline-block';
-			carrito.style.display = 'inline-block';
-			if (usuario) usuario.style.display = 'inline-block';
+			busquedaInput.style.opacity = '0';
+			busquedaBoton.style.opacity = '0';
+			setTimeout(() => {
+				busquedaInput.style.display = 'none';
+				busquedaBoton.style.display = 'none';
+				busquedaMostrar.style.display = 'inline-block';
+				carrito.style.display = 'inline-block';
+				if (usuario) usuario.style.display = 'inline-block';
+				busquedaIcono.style.display = 'block';
+				busquedaIcono.style.transform = 'rotate(0) scaleX(1) translateX(0)';
+				busquedaIcono.style.backgroundColor = "transparent";
+			}, 600);
 		}, 100);
 	});
 
 	busquedaForm.addEventListener('submit', (e) => {
 		e.preventDefault();
-		console.log(busquedaInput.value);
+		// console.log(busquedaInput.value);
 		// TODO:
 		// * Hacer una request al backend
 		// * Redireccionar a /search?term donde se muestren todos los productos que cumplan con la regex
@@ -43,7 +69,7 @@ function handleBusqueda() {
 function mostrarBotonUsuario(){
 	// TODO:
 	// * Chequear si es admin
-	// * Mostrar u ocultar el boton de usuario en la navbar
+	// * Mostrar u ocultar el boton de usuario en la navbar dependiendo si es admin
 }
 
 function iniciarCarouselOwl(){
