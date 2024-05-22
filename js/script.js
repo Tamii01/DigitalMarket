19f6ec2652a0215a250386d068092d759080ed6b
unknown <tamii.pucheta1901@gmail.com>
2024-05-22 20:05:17 -0300

    cambios temps en contacto, validacion, etc

--- js/script.js
+++ js/script.js
@@ -4,24 +4,40 @@ const email = document.getElementById("email")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 
-form.addEventListener("submit", e=>{
-  e.preventDefault()
-  let warnings = ""
-  let entrar = false
-  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-  parrafo.innerHTML = ""
-  if(nombre.value.length <2){
-      warnings += `El nombre no es valido <br>`
-      entrar = true
-  }
-  if(!regexEmail.test(email.value)){
-      warnings += `El email no es valido <br>`
-      entrar = true
-  }
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    let regexNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+    parrafo.innerHTML = ""
 
-  if(entrar){
-      parrafo.innerHTML = warnings
-  }else{
-      parrafo.innerHTML = "<br> Enviado"
-  }
+    if (nombre.value === null || nombre.value.trim() === '') {
+        warnings += `El campo nombre no debe estar vacío <br>`
+        entrar = true
+    }
+    else if (nombre.value.length <= 2) {
+        warnings += `El nombre no es válido <br>`
+        entrar = true
+    }
+    else if(!regexNombre.test(nombre.value)){
+        warnings += `El nombre no debe contener números ni símbolos <br>`
+        entrar = true
+    }
+
+    if (!regexEmail.test(email.value)) {
+        warnings += `El email no es válido <br>`
+        entrar = true
+    }
+
+    if (entrar) {
+        parrafo.innerHTML = warnings
+    } else {
+        parrafo.innerHTML = "<br> Enviado"
+        setTimeout(() => {
+            parrafo.innerHTML = ""; // Limpia el mensaje después de 10 segundos
+        }, 5000); // 5000 milisegundos = 5 segundos
+        form.reset(); // Opcional: para resetear el formulario después de enviarlo
+    }
+
 })
