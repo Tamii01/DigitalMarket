7c9a83cd3f482c157365e5961ce20811c723aa60
unknown <tamii.pucheta1901@gmail.com>
2024-05-22 03:39:53 -0300

    Cambio de diseño, espacio de abajo eliminado, boton sin borde, js validado

--- js/script.js
+++ js/script.js
@@ -1,62 +1,27 @@
-const form = document.querySelector("form"),
-fileInput = document.querySelector(".file-input"),
-progressArea = document.querySelector(".progress-area"),
-uploadedArea = document.querySelector(".uploaded-area");
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
 
-form.addEventListener("click", () =>{
-  fileInput.click();
-});
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-fileInput.onchange = ({target})=>{
-  let file = target.files[0];
-  if(file){
-    let fileName = file.name;
-    if(fileName.length >= 12){
-      let splitName = fileName.split('.');
-      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-    }
-    uploadFile(fileName);
+form.addEventListener("submit", e=>{
+  e.preventDefault()
+  let warnings = ""
+  let entrar = false
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+  parrafo.innerHTML = ""
+  if(nombre.value.length <2){
+      warnings += `El nombre no es valido <br>`
+      entrar = true
   }
-}
+  if(!regexEmail.test(email.value)){
+      warnings += `El email no es valido <br>`
+      entrar = true
+  }
 
-function uploadFile(name){
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "php/upload.php");
-  xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-    let fileLoaded = Math.floor((loaded / total) * 100);
-    let fileTotal = Math.floor(total / 1000);
-    let fileSize;
-    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-    let progressHTML = `<li class="row">
-                          <i class="fas fa-file-alt"></i>
-                          <div class="content">
-                            <div class="details">
-                              <span class="name">${name} • Uploading</span>
-                              <span class="percent">${fileLoaded}%</span>
-                            </div>
-                            <div class="progress-bar">
-                              <div class="progress" style="width: ${fileLoaded}%"></div>
-                            </div>
-                          </div>
-                        </li>`;
-    uploadedArea.classList.add("onprogress");
-    progressArea.innerHTML = progressHTML;
-    if(loaded == total){
-      progressArea.innerHTML = "";
-      let uploadedHTML = `<li class="row">
-                            <div class="content upload">
-                              <i class="fas fa-file-alt"></i>
-                              <div class="details">
-                                <span class="name">${name} • Uploaded</span>
-                                <span class="size">${fileSize}</span>
-                              </div>
-                            </div>
-                            <i class="fas fa-check"></i>
-                          </li>`;
-      uploadedArea.classList.remove("onprogress");
-      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-    }
-  });
-  let data = new FormData(form);
-  xhr.send(data);
-}
+  if(entrar){
+      parrafo.innerHTML = warnings
+  }else{
+      parrafo.innerHTML = "<br> Enviado"
+  }
+})
